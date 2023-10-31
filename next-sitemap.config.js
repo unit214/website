17d5a47b80879d8d7a69fe56720a9585a9ee057d
Timo Erdelt <timo@unit214.de>
2023-10-31 15:35:44 +0100

    fix: use correct site url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,8 +3,8 @@
  * @see https://github.com/iamvishnusankar/next-sitemap#readme
  */
 module.exports = {
-  /** Without additional '/' on the end, e.g. https://unit214.xyz */
-  siteUrl: 'https://unit214.xyz',
+  /** Without additional '/' on the end, e.g. https://unit214.dev */
+  siteUrl: 'https://unit214.dev',
   generateRobotsTxt: true,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],
