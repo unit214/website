3641e37bca57543d1b1b4cc275f7903fc37ed3a6
Timo Erdelt <timo@unit214.de>
2023-11-09 14:51:37 +0100

    feat!: add cyan box with main claim and sticky header

---
+++ src/components/MainClaim.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+export default function MainClaim() {
+  return (
+    <div className='ml-28 flex flex-col gap-2.5 font-mono text-4xl font-extrabold text-white'>
+      <span>
+        <span className='text-white/50'> UNLOCK YOUR </span> POTENTIAL
+      </span>
+      <span>
+        <span className='text-white/50'> WITH OUR </span> FULL STACK DEVELOPMENT
+      </span>
+      <span>
+        <span className='text-white/50'> AND </span> IT CONSULTING
+      </span>
+    </div>
+  );
+}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,37 @@
 import clsx, { ClassValue } from 'clsx';
+import { MutableRefObject, useEffect, useState } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 /** Merge classes with tailwind-merge with clsx full feature */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function useDetectSticky(ref: MutableRefObject<Element | null>) {
+  const [isSticky, setIsSticky] = useState(false);
+
+  // mount
+  useEffect(() => {
+    if (ref.current) {
+      const cachedRef = ref.current,
+        observer = new IntersectionObserver(
+          ([e]) => {
+            // Only set isSticky when the top part gets invisible and not the bottom part
+            setIsSticky(
+              e.intersectionRatio < 1 && e.intersectionRect.top === 0
+            );
+          },
+          { threshold: [1] }
+        );
+
+      observer.observe(cachedRef);
+
+      // unmount
+      return () => {
+        observer.unobserve(cachedRef);
+      };
+    }
+  }, [ref]);
+
+  return [isSticky];
 }
