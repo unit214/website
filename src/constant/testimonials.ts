4ddf4d233a10aec8da91f645c3f4737e13bd9a90
Timo Erdelt <timo@unit214.de>
2023-11-17 10:58:23 +0100

    feat: updated testimonial text sources

--- src/constant/testimonials.ts
+++ src/constant/testimonials.ts
@@ -2,7 +2,7 @@ import { Testimonial } from '@/constant/models';
 
 export const TESTIMONIALS: Testimonial[] = [
   {
-    source: 'Spendit AG',
+    source: 'Andreas Lehmann - Head of Engineering at Spendit AG',
     text: 'Spendit and Unit 214 are collaborating with great success in different projects. Unit 214 delivered a scalable Scala microservice, provided an initial setup for our Kubernetes setup, as well as supported our internal development team with their expertise. Overall their efforts exceeded our expectations.',
   },
   {
@@ -14,11 +14,11 @@ export const TESTIMONIALS: Testimonial[] = [
     text: 'We enjoy working with Unit 214 a lot as they are able to build software end-to-end in a very independent and professional way, always managing expectations pro-actively. At the same time integrating them into our existing team was super smooth as well.',
   },
   {
-    source: 'Ape Unit',
+    source: 'Emil Wagner - Managing Director at Ape Unit',
     text: 'Unit 214 has been a reliable partner for years, they enable us to add expert capacity in a wide range of blockchain projects, from smart contract development over Web3 integration to the conception of an NFT sale and ticketing-system. With the start of the AI-era their specialty consultant has provided us with in-depth knowledge and their senior team has helped us to successfully launch an AI-based product.',
   },
   {
-    source: 'Unit A',
+    source: 'Moritz Stellmacher - Unit A',
     text: "The senior full-stack engineers at Unit 214 were vital mentors to our newly formed team. Their support accelerated our team's growth, delivery timelines, and code quality with a lasting impact.",
   },
 ];
