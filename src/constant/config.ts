507b6e88626c2de6a7c14a0eda3fa61fe0ddb3c6
Timo Erdelt <timo@unit214.de>
2023-11-20 17:06:00 +0100

    feat(og): add open graph preview image, adjust title and description

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,6 +1,7 @@
 export const siteConfig = {
-  title: 'Unit 214',
-  description: 'Full-Stack Development and IT Consulting',
+  title: 'Unit 214 | Full-Stack Development and IT Consulting',
+  description:
+    "With our experience as well as our transparent and flexible work, we always meet our clients' needs by providing Full-Stack Development, Software Architecture and IT Consulting.",
   /** Without additional '/' on the end, e.g. https://unit214.dev */
   url: 'https://unit214.dev',
 };
