b90dd1fa8d3be3d1c1844a879f771cbbac342628
Timo Erdelt <timo@unit214.de>
2023-11-20 15:25:18 +0100

    feat(header): add amber box to fixed/sticky part of header

--- src/components/AmberBox.tsx
+++
@@ -1,5 +0,0 @@
-export default function AmberBox() {
-  return (
-    <div className='bg-primary-amber custom-angled-rectangle-amber-header absolute h-[700px] w-full justify-center lg:h-[824px]' />
-  );
-}
