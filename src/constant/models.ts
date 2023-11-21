0aeea88eecee37e4cbbaa9c3efb6477021ffd999
Timo Erdelt <timo@unit214.de>
2023-11-21 15:40:23 +0100

    feat(testimonials): implement swiper, add url, increase text size

--- src/constant/models.ts
+++ src/constant/models.ts
@@ -1,4 +1,11 @@
 export type Testimonial = {
   source: string;
   text: string;
+  logo?: string;
+  website?: TestimonialWebsite;
+};
+
+export type TestimonialWebsite = {
+  text: string;
+  url: string;
 };
--- src/constant/testimonials.ts
+++ src/constant/testimonials.ts
@@ -2,29 +2,43 @@ import { Testimonial } from '@/constant/models';
 
 export const TESTIMONIALS: Testimonial[] = [
   {
-    source: 'Andreas Lehmann - Head of Engineering at Spendit AG',
+    source: 'Andreas Lehmann - Head of Engineering at',
     text: 'Spendit and Unit 214 are collaborating with great success in different projects. Unit 214 delivered a scalable Scala microservice, provided an initial setup for our Kubernetes setup, as well as supported our internal development team with their expertise. Overall their efforts exceeded our expectations.',
+    logo: '/svg/logo-spendit.svg',
+    website: {
+      text: 'Spendit AG',
+      url: 'https://www.spendit.de',
+    },
+  },
+  {
+    source: 'Mattes Groeger - CTO & Co-Founder of',
+    text: 'We enjoy working with Unit 214 a lot as they are able to build software end-to-end in a very independent and professional way, always managing expectations pro-actively. At the same time integrating them into our existing team was super smooth as well.',
+    logo: '/svg/logo-atem.svg',
+    website: {
+      text: 'Atem.Earth',
+      url: 'https://www.atem.earth',
+    },
+  },
+  {
+    source: 'Emil Wagner - Managing Director at',
+    text: 'Unit 214 has been a reliable partner for years, they enable us to add expert capacity in a wide range of blockchain projects, from smart contract development over Web3 integration to the conception of an NFT sale and ticketing-system. With the start of the AI-era their specialty consultant has provided us with in-depth knowledge and their senior team has helped us to successfully launch an AI-based product.',
+    logo: '/images/logo-apeunit.png',
+    website: {
+      text: 'Ape Unit',
+      url: 'https://apeunit.com',
+    },
+  },
+  {
+    source: 'Moritz Stellmacher -',
+    text: "The senior full-stack engineers at Unit 214 were vital mentors to our newly formed team. Their support accelerated our team's growth, delivery timelines, and code quality with a lasting impact.",
+    logo: '/images/logo-unita.png',
+    website: {
+      text: ' Unit A',
+      url: 'https://unita.apeunit.com',
+    },
   },
   {
     source: 'Yanislav Malahov',
     text: 'The team at Unit 214 has a long and successful history of working with us, they have been a reliable partner in many different engineering challenges. Unit 214 has been a key resource for technical analysis, system architecture, project planning and full-stack blockchain development.',
   },
-  {
-    source: 'Mattes Groeger - CTO & Co-Founder of Atem.Earth',
-    text: 'We enjoy working with Unit 214 a lot as they are able to build software end-to-end in a very independent and professional way, always managing expectations pro-actively. At the same time integrating them into our existing team was super smooth as well.',
-  },
-  {
-    source: 'Emil Wagner - Managing Director at Ape Unit',
-    text: 'Unit 214 has been a reliable partner for years, they enable us to add expert capacity in a wide range of blockchain projects, from smart contract development over Web3 integration to the conception of an NFT sale and ticketing-system. With the start of the AI-era their specialty consultant has provided us with in-depth knowledge and their senior team has helped us to successfully launch an AI-based product.',
-  },
-  {
-    source: 'Moritz Stellmacher - Unit A',
-    text: "The senior full-stack engineers at Unit 214 were vital mentors to our newly formed team. Their support accelerated our team's growth, delivery timelines, and code quality with a lasting impact.",
-  },
-];
-
-export const TESTIMONIAL_LOGOS: string[] = [
-  '/svg/logo-spendit.svg',
-  '/svg/logo-atem.svg',
-  '/images/logo-apeunit.png',
 ];
