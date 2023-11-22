e4564e7704fe8d0dc920d78f5088088b4bfd5ea7
Timo Erdelt <timo@unit214.de>
2023-11-22 10:23:51 +0100

    feat(buttons): add hover opacity effect

--- src/components/LetsGetInTouch.tsx
+++ src/components/LetsGetInTouch.tsx
@@ -8,7 +8,7 @@ export default function LetsGetInTouch() {
   return (
     <div className='mb-8 mt-14 lg:mb-28 lg:mt-24'>
       <NextLink
-        className='flex w-fit items-center justify-center gap-3 rounded-full bg-black px-6 py-3 text-white no-underline lg:px-8'
+        className='flex w-fit items-center justify-center gap-3 rounded-full bg-black px-6 py-3 text-white no-underline transition-opacity duration-200 ease-in hover:opacity-70 lg:px-8'
         href={`mailto:${contactEmail}`}
       >
         <span className='font-primary text-2xl font-semibold lg:text-3xl'>
