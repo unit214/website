eb02bb83425fbc53edb373830e603f3fe133a422
Timo Erdelt <timo@unit214.de>
2023-11-22 14:26:08 +0100

    feat(analytics): add analytics using goatcounter

---
+++ src/components/GoatCounter.tsx
@@ -0,0 +1,44 @@
+'use client';
+import { usePathname } from 'next/navigation';
+import Script from 'next/script';
+import { useEffect, useState } from 'react';
+
+export function GoatCounter() {
+  const pathname = usePathname();
+  const [goatcounter, setGoatcounter] = useState<GoatCounter | null>(null);
+
+  useEffect(() => {
+    if (!goatcounter) {
+      return;
+    }
+
+    goatcounter.count({
+      path: pathname,
+    });
+  }, [pathname, goatcounter]);
+
+  return (
+    <Script
+      strategy='afterInteractive'
+      data-goatcounter='https://unit214.goatcounter.com/count'
+      src='https://gc.zgo.at/count.js'
+      // data-goatcounter-settings='{"allow_local": true}'
+      onLoad={() => {
+        if (!window.goatcounter) {
+          throw new Error('goatcounter object is not defined');
+        }
+        setGoatcounter(window.goatcounter);
+      }}
+    />
+  );
+}
+
+interface GoatCounter {
+  count: (options: { path: string }) => void;
+}
+
+declare global {
+  interface Window {
+    goatcounter: GoatCounter;
+  }
+}
