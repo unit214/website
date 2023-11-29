d16e044b721249e2ce2dbd7ea60a0c3641ec077c
Timo Erdelt <timo@unit214.de>
2023-11-29 10:07:25 +0100

    feat(404): adjust not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,8 @@
+'use client';
 import { Metadata } from 'next';
-import { RiAlarmWarningFill } from 'react-icons/ri';
+
+import NextImage from '@/components/NextImage';
+import NextLink from '@/components/NextLink';
 
 export const metadata: Metadata = {
   title: 'Not Found',
@@ -10,12 +13,15 @@ export default function NotFound() {
     <main>
       <section className='bg-white'>
         <div className='layout flex min-h-screen flex-col items-center justify-center text-center text-black'>
-          <RiAlarmWarningFill
-            size={60}
-            className='drop-shadow-glow animate-flicker text-red-500'
+          <NextImage
+            src='/svg/unit214.svg'
+            alt='unit214-logo'
+            width={120}
+            height={0}
+            useSkeleton
           />
-          <h1 className='mt-8 text-4xl md:text-6xl'>Page Not Found</h1>
-          <a href='/'>Back to home</a>
+          <h1 className='mb-8 mt-8 text-4xl lg:text-6xl'>Page Not Found</h1>
+          <NextLink href='/'>Back to home</NextLink>
         </div>
       </section>
     </main>
