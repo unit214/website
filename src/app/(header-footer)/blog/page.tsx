fa82684bbca2525d714c23a935e553656849fa9f
Timo Erdelt <timo@unit214.de>
2024-01-31 17:04:44 +0100

    refactor: make /blog redirect to latest blogpost instead of footer deciding it itself

--- src/app/(header-footer)/blog/page.tsx
+++ src/app/(header-footer)/blog/page.tsx
@@ -1,5 +1,16 @@
-import { notFound } from 'next/navigation'
+import { notFound, redirect } from 'next/navigation'
 
-export default function BlogPage() {
-  return notFound()
+import { getPosts } from '@/lib/blog'
+
+export default async function BlogPage() {
+  const allPosts = await getPosts()
+  let mostRecentPostId: string | undefined
+  if (allPosts && allPosts.length > 0) {
+    mostRecentPostId = allPosts[0].meta.id
+  }
+  if (mostRecentPostId) {
+    redirect(`blog/${mostRecentPostId}`)
+  } else {
+    return notFound()
+  }
 }
